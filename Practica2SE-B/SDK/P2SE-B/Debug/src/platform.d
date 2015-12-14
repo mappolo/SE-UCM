83aac7e953693db2c5dfdf4c011a94e4de98ba06
Miguel A. Perez <mipere01@ucm.es>
2015-12-14 13:08:51 +0100

    Practica 2 B Funcionando

---
+++ Practica2SE-B/SDK/P2SE-B/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d: ../src/platform.c \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xparameters.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xil_cache.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xil_types.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xparameters.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xil_cache.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xil_types.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
